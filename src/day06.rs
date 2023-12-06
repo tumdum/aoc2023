71026e5e47ebbc443983df2137412bdcf45b190b
Tomasz Kłak <tomasz@tomaszklak.pl>
2023-12-06 06:53:42 +0100

    Day06 speedup

--- src/day06.rs
+++ src/day06.rs
@@ -1,11 +1,47 @@
 use anyhow::Result;
-use std::time::{Duration, Instant};
+use std::{
+    cmp::Ordering,
+    time::{Duration, Instant},
+};
 
 use crate::input::token_groups;
 
+fn binary_search_by<F>(first: i64, last: i64, mut f: F) -> Result<i64, i64>
+where
+    F: FnMut(i64) -> Ordering,
+{
+    use Ordering::*;
+    let mut size = last - first + 1;
+    let mut left = first;
+    let mut right = last + 1;
+    while left < right {
+        let mid = left + size / 2;
+        match f(mid) {
+            Less => {
+                left = mid + 1;
+            }
+            Greater => {
+                right = mid;
+            }
+            Equal => return Ok(mid),
+        }
+
+        size = right - left;
+    }
+
+    Err(left)
+}
+
+fn either_way<T>(r: Result<T, T>) -> T {
+    match r {
+        Ok(v) => v,
+        Err(v) => v,
+    }
+}
+
 fn ways_to_win_race(t: i64, record: i64) -> i64 {
-    let first_win = (0..t).position(|p| ((t - p) * p) > record).unwrap() as i64;
-    let last_win = t - (0..t).rev().position(|p| ((t - p) * p) > record).unwrap() as i64;
+    let first_win = either_way(binary_search_by(0, t / 2, |p| ((t - p) * p).cmp(&record)));
+    let last_win = either_way(binary_search_by(t / 2, t, |p| record.cmp(&((t - p) * p))));
 
     last_win - first_win
 }
