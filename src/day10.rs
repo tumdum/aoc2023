564169ae8d704633be17efa49bb190f2dcc39adb
Tomasz Kłak <tomasz@tomaszklak.pl>
2023-12-10 09:30:54 +0100

    Day10 speedup

--- src/day10.rs
+++ src/day10.rs
@@ -1,55 +1,42 @@
 use anyhow::Result;
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::FxHashSet;
+use smallvec::{smallvec, SmallVec};
 use std::time::{Duration, Instant};
 
 use crate::input::tokens;
 
 type Map = Vec<Vec<char>>;
-type PosToAllowed = FxHashMap<char, Vec<Pos>>;
-type Pos = (i64, i64);
+type PosToAllowed = Vec<SmallVec<[(Pos, SmallVec<[char; 3]>); 4]>>;
+type Pos = (i16, i16);
 
-fn get(x: i64, y: i64, m: &Map) -> Option<char> {
+fn get(x: i16, y: i16, m: &Map) -> Option<char> {
     m.get(y as usize)
         .and_then(|row| row.get(x as usize))
         .copied()
 }
 
-fn get_around(x: i64, y: i64, m: &Map, p_to_delta: &PosToAllowed) -> Vec<Pos> {
-    let mut ret = vec![];
+fn get_around(x: i16, y: i16, m: &Map, p_to_delta: &PosToAllowed) -> SmallVec<[Pos; 4]> {
+    let mut ret = smallvec![];
     let p = get(x, y, m).unwrap();
-    let allowed = p_to_delta.get(&p).unwrap();
-    for (dx, dy) in allowed {
+    let allowed = &p_to_delta[p as usize];
+    for ((dx, dy), allowed_chars) in allowed {
         if let Some(current) = get(x + dx, y + dy, m) {
-            let mut ok = false;
-            if *dx == -1 && (current == '-' || current == 'L' || current == 'F') {
-                ok = true;
-            }
-            if *dx == 1 && (current == '-' || current == 'J' || current == '7') {
-                ok = true;
-            }
-            if *dy == -1 && (current == '|' || current == 'F' || current == '7') {
-                ok = true;
-            }
-            if *dy == 1 && (current == '|' || current == 'J' || current == 'L') {
-                ok = true;
-            }
-            if ok {
+            if allowed_chars.contains(&current) {
                 ret.push((x + dx, y + dy));
             }
         }
     }
     ret
 }
-fn get_around2(x: i64, y: i64, m: &Map, p_to_delta: &PosToAllowed) -> (Vec<Pos>, bool) {
-    let mut ret = vec![];
+fn get_around_dot(x: i16, y: i16, m: &Map) -> (SmallVec<[Pos; 4]>, bool) {
+    let mut ret = smallvec![];
     let mut reached_outside = false;
-    let p = get(x, y, m).unwrap();
-    let allowed = p_to_delta.get(&p).unwrap();
-    for (dx, dy) in allowed {
+
+    for (dx, dy) in [(-1, 0), (1, 0), (0, 1), (0, -1)] {
         let x = x + dx;
         let y = y + dy;
 
-        if x < 0 || y < 0 || x == m[0].len() as i64 || y == m.len() as i64 {
+        if x < 0 || y < 0 || x == m[0].len() as i16 || y == m.len() as i16 {
             reached_outside = true;
         }
         if let Some(current) = get(x, y, m) {
@@ -63,21 +50,21 @@ fn get_around2(x: i64, y: i64, m: &Map, p_to_delta: &PosToAllowed) -> (Vec<Pos>,
 }
 
 fn get_next(
-    x: i64,
-    y: i64,
+    x: i16,
+    y: i16,
     m: &Map,
-    seen: &FxHashSet<Pos>,
+    seen: &[Vec<bool>],
     p_to_delta: &PosToAllowed,
-) -> Option<(i64, i64)> {
+) -> Option<(i16, i16)> {
     for p in get_around(x, y, m, p_to_delta) {
-        if !seen.contains(&p) {
+        if !seen[p.1 as usize][p.0 as usize] {
             return Some(p);
         }
     }
     None
 }
 
-fn flood_fill(start: Pos, m: &Map, p_to_delta: &PosToAllowed) -> (FxHashSet<Pos>, bool) {
+fn flood_fill(start: Pos, m: &Map) -> (FxHashSet<Pos>, bool) {
     let mut not_in_loop: FxHashSet<Pos> = Default::default();
     let mut todo: FxHashSet<Pos> = Default::default();
     todo.insert(start);
@@ -86,7 +73,7 @@ fn flood_fill(start: Pos, m: &Map, p_to_delta: &PosToAllowed) -> (FxHashSet<Pos>
         let next: Pos = *todo.iter().next().unwrap();
         todo.remove(&next);
         not_in_loop.insert(next);
-        let (cands, outside) = get_around2(next.0, next.1, &m, &p_to_delta);
+        let (cands, outside) = get_around_dot(next.0, next.1, &m);
         if outside {
             reached_outside = true;
         }
@@ -161,26 +148,34 @@ pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duratio
     for (y, row) in input.iter().enumerate() {
         for (x, val) in row.iter().enumerate() {
             if *val == 'S' {
-                start = (x as i64, y as i64);
-                // break 'l1;
+                start = (x as i16, y as i16);
             }
             if *val == '.' {
-                dots.insert((x as i64, y as i64));
+                dots.insert((x as i16, y as i16));
             }
         }
     }
 
-    let mut seen: FxHashSet<(i64, i64)> = FxHashSet::default();
-    seen.insert(start);
-    let mut p_to_delta: PosToAllowed = Default::default();
-    p_to_delta.insert('|', vec![(0, 1), (0, -1)]);
-    p_to_delta.insert('-', vec![(1, 0), (-1, 0)]);
-    p_to_delta.insert('L', vec![(0, -1), (1, 0)]);
-    p_to_delta.insert('J', vec![(0, -1), (-1, 0)]);
-    p_to_delta.insert('7', vec![(0, 1), (-1, 0)]);
-    p_to_delta.insert('F', vec![(1, 0), (0, 1)]);
-    p_to_delta.insert('S', vec![(1, 0), (-1, 0), (0, 1), (0, -1)]);
-    p_to_delta.insert('.', vec![(1, 0), (-1, 0), (0, 1), (0, -1)]);
+    let mut seen: Vec<Vec<bool>> = vec![vec![false; input[0].len()]; input.len()];
+    seen[start.1 as usize][start.0 as usize] = true;
+    let mut p_to_delta: PosToAllowed = vec![smallvec![]; 256];
+    let l = smallvec!['-', 'L', 'F'];
+    let r = smallvec!['-', 'J', '7'];
+    let u = smallvec!['|', '7', 'F'];
+    let d = smallvec!['|', 'L', 'J'];
+
+    p_to_delta['|' as usize] = smallvec![((0, 1), d.clone()), ((0, -1), u.clone())];
+    p_to_delta['-' as usize] = smallvec![((1, 0), r.clone()), ((-1, 0), l.clone())];
+    p_to_delta['L' as usize] = smallvec![((0, -1), u.clone()), ((1, 0), r.clone())];
+    p_to_delta['J' as usize] = smallvec![((0, -1), u.clone()), ((-1, 0), l.clone())];
+    p_to_delta['7' as usize] = smallvec![((0, 1), d.clone()), ((-1, 0), l.clone())];
+    p_to_delta['F' as usize] = smallvec![((1, 0), r.clone()), ((0, 1), d.clone())];
+    p_to_delta['S' as usize] = smallvec![
+        ((1, 0), r.clone()),
+        ((-1, 0), l.clone()),
+        ((0, 1), d.clone()),
+        ((0, -1), u.clone()),
+    ];
 
     let mut len = 0;
     let mut current = start;
@@ -189,7 +184,7 @@ pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duratio
         len += 1;
         match get_next(current.0, current.1, &input, &seen, &p_to_delta) {
             Some(p) => {
-                seen.insert(p);
+                seen[p.1 as usize][p.0 as usize] = true;
                 current = p;
             }
             None => {
@@ -201,9 +196,9 @@ pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duratio
 
     for y in 0..input.len() {
         for x in 0..input[y].len() {
-            if !seen.contains(&(x as i64, y as i64)) {
+            if !seen[y as usize][x as usize] {
                 input[y][x] = '.';
-                dots.insert((x as i64, y as i64));
+                dots.insert((x as i16, y as i16));
             }
         }
     }
@@ -213,7 +208,7 @@ pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duratio
     while !dots.is_empty() {
         let start = *dots.iter().next().unwrap();
 
-        let (not_in_loop, reached_outside) = flood_fill(start, &input, &p_to_delta);
+        let (not_in_loop, reached_outside) = flood_fill(start, &input);
 
         for p in &not_in_loop {
             dots.remove(p);
