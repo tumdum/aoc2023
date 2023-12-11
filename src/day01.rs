f6701cdd81714e77fe657b07daca386599f1c6b2
Tomasz Kłak <tomasz@tomaszklak.pl>
2023-12-11 08:24:20 +0100

    Day01 speedup

--- src/day01.rs
+++ src/day01.rs
@@ -1,41 +1,41 @@
 use anyhow::Result;
-use rustc_hash::FxHashMap;
 use std::time::{Duration, Instant};
 
 use crate::input::tokens;
 
 pub fn solve(input: &str, verify_expected: bool, output: bool) -> Result<Duration> {
-    let input: Vec<String> = tokens(input, None);
+    let lines: Vec<String> = tokens(input, None);
 
     let s = Instant::now();
 
     let parse = |s: &str| -> u32 {
-        let digs: Vec<_> = s.chars().filter(|c| c.is_digit(10)).collect();
-        digs[0].to_digit(10).unwrap() * 10 + digs[digs.len() - 1].to_digit(10).unwrap()
+        let mut i = s.chars().filter(|c| c.is_digit(10));
+        let first = i.next().unwrap().to_digit(10).unwrap();
+        match i.last().and_then(|c| c.to_digit(10)) {
+            Some(last) => first * 10 + last,
+            None => first * 10 + first,
+        }
     };
 
-    let part1: u32 = input.iter().map(|s| parse(s)).sum();
+    let part1: u32 = lines.iter().map(|s| parse(s)).sum();
 
-    let mut h: FxHashMap<_, _> = FxHashMap::default();
-    h.insert("one", "o1e");
-    h.insert("two", "t2o");
-    h.insert("three", "t3e");
-    h.insert("four", "f4r");
-    h.insert("five", "f5e");
-    h.insert("six", "s6x");
-    h.insert("seven", "s7n");
-    h.insert("eight", "e8t");
-    h.insert("nine", "n9e");
+    let mut h = vec![];
+    h.push(("one", "o1e"));
+    h.push(("two", "t2o"));
+    h.push(("three", "t3e"));
+    h.push(("four", "f4r"));
+    h.push(("five", "f5e"));
+    h.push(("six", "s6x"));
+    h.push(("seven", "s7n"));
+    h.push(("eight", "e8t"));
+    h.push(("nine", "n9e"));
 
-    let part2: u32 = input
-        .into_iter()
-        .map(|mut s| {
-            for (k, v) in &h {
-                s = s.replace(k, v);
-            }
-            parse(&s)
-        })
-        .sum();
+    let mut input = input.to_owned();
+    for (k, v) in &h {
+        input = input.replace(k, v);
+    }
+
+    let part2: u32 = input.lines().map(|s| parse(&s)).sum();
 
     let e = s.elapsed();
 
